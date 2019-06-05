b3e246a8099f641e86152cdae456b54df2320c66
sejinkay <sejinkay@gmail.com>
2019-06-05 14:53:43 +0100

    added test for Sulfuras and Backstage passes

--- glided_rose/spec/glided_rose_spec.rb
+++ glided_rose/spec/glided_rose_spec.rb
@@ -15,6 +15,21 @@ describe GildedRose do
       GildedRose.new(items).update_quality()
       expect(items[0].quality).to eq 2
     end
+
+    it "Sulfuras never has to be sold or decreases in Quality" do
+      items = [Item.new("Sulfuras, Hand of Ragnaros", 1, 80)]
+      GildedRose.new(items).update_quality()
+      GildedRose.new(items).update_quality()
+      expect(items[0].sell_in).to eq 1
+      expect(items[0].quality).to eq 80
+    end
+
+    it "Backstage passes quality drops to 0 after the concert" do
+      items = [Item.new("Backstage passes to a TAFKAL80ETC concert", 1, 20)]
+      GildedRose.new(items).update_quality()
+      GildedRose.new(items).update_quality()
+      expect(items[0].quality).to eq 0
+    end
   end
 
 end
